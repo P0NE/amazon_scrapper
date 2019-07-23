40daf281ba6bc71f158acc09013c8630c92f0eb7
Mathieu <math.snabre@gmail.com>
2019-07-23 10:49:15 +0200

    first working version

--- script/__main__.py
+++ script/__main__.py
@@ -1,17 +1,28 @@
 import csv
 import asyncio
 import requests
+import os
+import yagmail
+import keyring
+from dotenv import load_dotenv
 from bs4 import BeautifulSoup
+
+load_dotenv()
+yagmail.register(os.getenv('EMAIL_ADDRESS'),
+                 os.getenv('EMAIL_PASSWORD'))
 
 
 @asyncio.coroutine
 def check_price(check):
 
-    # header = {
-    #     'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'}
-    # page = requests.get(check['url'], headers=header)
-    # soup = BeautifulSoup(page.content, 'html.parser')
-    print(check)
+    page = requests.get(check['url'], headers={
+                        "User-Agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'})
+    soup = BeautifulSoup(page.content, 'html.parser')
+    price = soup.find(id="price_inside_buybox").get_text()
+
+    if price < check['price']:
+        yagmail.SMTP(os.getenv('EMAIL_ADDRESS')).send(
+            check['mail'], 'Price LOWER !!!!', 'The price for {0} is lower than wanted !!!!'.format(check['url']))
 
 
 @asyncio.coroutine
